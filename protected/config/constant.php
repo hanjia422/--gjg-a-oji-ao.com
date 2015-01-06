a27b4da6fe48811b460587a3f077e60dbe92230a
xiaoyuxiaoyuimpr <599908644@qq.com>
2015-01-06 16:16:15 +0800

    xiaoyu

--- protected/config/constant.php
+++ protected/config/constant.php
@@ -3,7 +3,7 @@
 /**
  * 资源文件路径常量
  */
-define("SITE_URL",'http://localhost/back.com/');//设置site地址
+define("SITE_URL",'http://localhost/--gjg-a-oji-ao.com/');//设置site地址
 
 define("IMG_URL",SITE_URL."assets/img/");
 define("JS_URL",SITE_URL."assets/js/");
